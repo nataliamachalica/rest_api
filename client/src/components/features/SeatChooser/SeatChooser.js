d46976181cc3cfd2001692391f45fc138208bc1f
Natalia Szlapka <n.machalica@gmail.com>
2021-09-12 17:11:12 +0200

    Add socket.io

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Button, Progress, Alert } from 'reactstrap';
+import io from 'socket.io-client';
 
 import './SeatChooser.scss';
 
 class SeatChooser extends React.Component {
 
   componentDidMount() {
+    this.socket = io((process.env.NODE_ENV === 'production') ? '' : 'localhost:8000', {transports: ["websocket"]});
+
     const { loadSeats } = this.props;
     loadSeats();
     setInterval(loadSeats, 120000);
   }
+
 
   componentWillUnmount(){
     const { loadSeats } = this.props;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const socket = require('socket.io');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -26,6 +27,16 @@ app.use((req, res) => {
 	res.status(404).send('404 not found' );
 })
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 3000');
+});
+
+const io = socket(server, {
+	cors: {
+		origin: '*'
+	}
+});
+
+io.on('connection', (socket) => {
+	console.log('New socket! ' + socket.id);
 });
